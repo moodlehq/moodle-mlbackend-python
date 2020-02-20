ac5536bb5dacd20602dc9024caf2c281791aa8ed
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2020-02-21 12:28:40 +1300

    webapp: debug option to stash incoming request data
    If the MOODLE_ML_STASH_DIR is set, details of incoming requests are
    stored in the specified directory. The data stored is sufficient to
    replay the request in a test environment.
    
    Forthcoming commits will introduce tools to inspect and analyse these
    stashed requests, anonymise their contents so that derived data can be
    publicly shared, and add tests based on this data.

--- moodlemlbackend/webapp/access.py
+++ moodlemlbackend/webapp/access.py
@@ -6,11 +6,41 @@ from functools import wraps
 from flask import request
 
 
+STASH_DIR = os.environ.get('MOODLE_ML_STASH_DIR')
+
+
+def stash_data(*args, **kwargs):
+    import pickle, time, random
+    reqdir = os.path.join(STASH_DIR, 'requests')
+    os.makedirs(reqdir, exist_ok=True)
+    t = time.strftime('%Y-%m-%d-%H-%M-%S-')
+    s = os.path.join(reqdir, t + re.sub(r'\W', '_', request.path))
+    try:
+        f = open(s, 'xb')
+    except FileExistsError:
+        for i in range(1, 10):
+            s2 = f'{s}.{i}'
+            try:
+                f = open(s2, 'xb')
+                break
+            except FileExistsError:
+                continue
+    r = {
+        'url':   request.url,
+        'data':  request.get_data(),
+        'headers': request.headers.to_wsgi_list()
+    }
+    pickle.dump(r, f)
+    f.close()
+
+
 def check_access(f):
     '''Checks the access to the route.'''
 
     @wraps(f)
     def access_wrapper(*args, **kwargs):
+        if STASH_DIR is not None:
+            stash_data(*args, **kwargs)
 
         # Check that the environment var is properly set.
         envvarname = "MOODLE_MLBACKEND_PYTHON_USERS"
